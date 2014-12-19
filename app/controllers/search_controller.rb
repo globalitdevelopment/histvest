e2850197650155acfd1f14d5de6af44483f5de5f
Loken Makwana <loken@globalit.no>
2014-12-19 16:08:09 +0100

    Ability to search people

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,10 +7,23 @@ class SearchController < ApplicationController
 			SearchTopic.increment(r.title)
 		end
 
+		results = @results.map do |r| 
+			{ value: r.value, label: r.label, avatar_path: r.avatar_path, url: topic_path(r) }
+		end
+
+		# there is no option for search from both so need to check in both firstname and lastname
+		Person.search(1, params[:term]).sample(5).each do |r|
+			results << { value: r.name, label: r.name, avatar_path: '/assets/rt-person-icon.png', url: r.url }
+		end
+
+		Person.search(1, nil, params[:term]).sample(5).each do |r|
+			results << { value: r.name, label: r.name, avatar_path: '/assets/rt-person-icon.png', url: r.url }
+		end
+
 		respond_to do |format|
 			format.json do
-				if @results.any?
-					render json: @results.to_json(:only => [:id], :methods => [:value, :label, :avatar_path])
+				if results.size > 0
+					render json: results
 				else
 					render json: [{id: nil, value: I18n.t('seach.no_results'), label: I18n.t('seach.no_results'), avatar_path: "/assets/rt-ukjent-icon.png"}].to_json
 				end
--- app/models/person.rb
+++ app/models/person.rb
@@ -9,31 +9,37 @@ class Person < ActiveRecord::Base
 		"http://digitalarkivet.arkivverket.no/ft/person/#{pfid}"
 	end
 
-	def self.search(page = 1)
+	def self.search(page=1, fornavn=nil, etternavn=nil)
 		vestfold = [ '0701','0702','0703','0704','0705','0706','0707',
 			'0711','0712','0713','0714','0715','0716','0717','0718','0719',
 			'0720','0721','0722','0723','0724','0725','0726','0727','0728',
 			'0798']
 		search_params = {
-			page: page,
-			k: vestfold.sample(5)
+			page: page
 		}
+		search_params[:fornavn] = fornavn if fornavn
+		search_params[:etternavn] = etternavn if etternavn
+		search_params[:k] = (fornavn || etternavn) ? vestfold : vestfold.sample(5)
+
 		search_url = "http://digitalarkivet.arkivverket.no/ft/sok/1910?#{search_params.to_query}"
 		puts search_url
 		doc = Nokogiri::XML(WrapperHelper::fetch_http(search_url)) 
 		doc.css("table.searchResultTable tbody tr").map do |tr|
 			pfid = tr.css('td a')[0].attribute('href').value.scan(/pf\d+/).first
-			next if Person.where(pfid: pfid).exists?
+			person = Person.find_by(pfid: pfid)
+			
+			unless person
+				person = self.new
+				person.pfid = pfid
+				person.name = tr.css('td')[0].text
+				person.date_of_birth = tr.css('td')[1].text
+				person.place_of_birth = tr.css('td')[2].text
+				person.description = tr.css('td')[3].text
+				person.location = Location.find_or_create_by! address: tr.css('td')[4].text
+				person.save!
+			end
 
-			person = self.new
-			person.pfid = pfid
-			person.name = tr.css('td')[0].text
-			person.date_of_birth = tr.css('td')[1].text
-			person.place_of_birth = tr.css('td')[2].text
-			person.description = tr.css('td')[3].text
-			person.location = Location.find_or_create_by! address: tr.css('td')[4].text
-
-			person.save!
+			person
 		end
 	end
 
