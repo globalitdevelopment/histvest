bb571bf58eb806608fa4ad04256c2651ff114964
Loken Makwana <loken@globalit.no>
2015-02-25 09:14:42 +0100

    Commented test routes

--- config/routes.rb
+++ config/routes.rb
@@ -59,8 +59,8 @@ Histvest::Application.routes.draw do
 
   get '/search', to: 'search#search'
 
-  get 'v2', to: 'pages#home'
-  get 'v2/topics', to: 'pages#topics'
+  #get 'v2', to: 'pages#home'
+  #get 'v2/topics', to: 'pages#topics'
   get 'v2/people', to: 'people#combined'
   
   # The priority is based upon order of creation:
