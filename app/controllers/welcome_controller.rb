04f6680546d33a9b88c80f546c6853f4783a98ee
Loken Makwana <lokenmakwana@yahoo.com>
2015-06-22 11:23:39 +0530

    Lazy load topics data for homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,21 +7,22 @@ class WelcomeController < ApplicationController
 	def index
 		@frequent_searches = SearchTopic.where('created_at > ?',Time.now - 7.days).limit(10).to_a
 		@topics = Topic.listed
-
 		@rand_topic = @topics.sample
-
-		@locations = Location.includes(:topics).joins(:topics).merge(@topics).to_gmaps4rails do |location, marker|												
-			marker.infowindow render_to_string(:partial => "/welcome/infowindow", :locals => { :topics => location.topics.map { |t| Topic.working_version(t) } })
-			marker.picture(picture: location.topics.size > 1 ? "http://www.googlemapsmarkers.com/v1/#{location.topics.size}/FD7567/" : "http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png")				
-			marker.json type: :topic, count: location.topics.size
-		end				
-
 		@front_page_article = Article.where(article_type: "front_page").first
 
-		# Render the site using the touch layout if the subdomain is touch.				
-		if touch?
-			render "touch_index", layout: "touch"
-		end		
+		respond_to do |format|
+			format.json {
+				@locations = Location.includes(:topics).joins(:topics).merge(@topics).to_gmaps4rails do |location, marker|												
+					marker.infowindow render_to_string(:partial => "/welcome/infowindow.html", :locals => { :topics => location.topics.map { |t| Topic.working_version(t) } })
+					marker.picture(picture: location.topics.size > 1 ? "http://www.googlemapsmarkers.com/v1/#{location.topics.size}/FD7567/" : "http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png")				
+					marker.json type: :topic, count: location.topics.size
+				end
+				render json: @locations
+			}
+			format.html {
+				render "touch_index", layout: "touch" if touch?
+			}
+		end
 	end
 
 	def take_another_topic    
--- config/routes.rb
+++ config/routes.rb
@@ -114,6 +114,7 @@ Histvest::Application.routes.draw do
   # just remember to delete public/index.html.
   
   root :to => 'welcome#index'
+  get 'home-topics', to: 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
   # This is a legacy wild controller route that's not recommended for RESTful applications.
